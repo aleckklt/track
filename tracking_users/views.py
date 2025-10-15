4ee3507aebd60c631bcbf9ea1c1591d0c0dd0094
aleckklt <aleckkouletio@gmail.com>
2025-10-15 13:05:29 +0100

    leger +

--- tracking_users/views.py
+++ tracking_users/views.py
@@ -7,57 +7,59 @@ from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.db.models import Max
 
-def home(request):
-    if not request.user.is_authenticated:
+def home (request):
+    if not request.user.is_authenticated :
         return redirect('login')
     return render(request, 'tracking_users/home.html')
 
 def user_login(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
+    if request.method == 'POST':
+        username=request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
+        login(request, user)
 
-        if user:
-            login(request, user)
+        if user :
             LoginHistory.objects.create(
                 user=user,
-                login_time=timezone.now()
+                login_time = timezone.now()
             )
             return redirect('home')
-        else:
-            return render(request, 'tracking_users/login.html', {'error': 'Identifiants incorrects.'})
-    return render(request, 'tracking_users/login.html')
+        else :
+            return render(request, 'tracking_users/login.html', {'error': 'Identifiants incorrects'})
+    return render (request, 'tracking_users/login.html')
 
 def user_logout(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated :
         last_login = LoginHistory.objects.filter(
-            user=request.user, logout_time__isnull=True
+            user=request.user,
+            logout_time__isnull = True
         ).last()
 
-        if last_login and last_login.login_time:
+        if last_login and last_login.login_time :
             last_login.logout_time = timezone.now()
             last_login.session_duration = last_login.logout_time - last_login.login_time
             last_login.save()
 
-    logout(request)
-    return redirect('login')
-
+        logout(request)
+        return redirect('login')
+    
 def login_history(request):
-    if not request.user.is_staff:
+    if not request.user.is_staff :
         return redirect('home')
     
-    latest_logins = (
-        LoginHistory.objects
-        .values('user')
-        .annotate(latest_login=Max('login_time'))
-    )
+    last_logins = (LoginHistory.objects
+                   .values('user')
+                   .annotate(last_login = Max('login_time'))
+                   )
 
-    latest_entries = LoginHistory.objects.filter(
-        login_time__in=[item['latest_login'] for item in latest_logins]
-    ).select_related('user').order_by('user__username')
-
-    return render(request, 'tracking_users/login_history.html', {'history': latest_entries})
+    latest_entries = (LoginHistory.objects
+                      .filter(login_time__in=[items['last_login'] for items in last_logins])
+                      .select_related('user')
+                      .order_by('user__username')
+                      )
+    
+    return render(request, 'tracking_users/login_history.html', {'history':latest_entries})
 
 def register_view(request):
     if request.method == 'POST':
@@ -98,10 +100,6 @@ def deactivate_user(request, user_id):
         return redirect('home')
 
     user = get_object_or_404(User, id=user_id)
-    if user == request.user:
-        messages.warning(request, "Vous ne pouvez pas vous désactiver vous-même .")
-        return redirect('login_history')
-
     user.is_active = False
     user.save()
     messages.warning(request, f"L'utilisateur {user.username} a été désactivé .")
